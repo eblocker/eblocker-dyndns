35fa3aad39ede8d2c5b118d29c6d59b3841756f0
eBlocker Open Source <dev@eblocker.org>
2024-08-14 09:37:55 +0200

    Add EUPL license

--- redisdyndns/redisdyndns.go
+++ redisdyndns/redisdyndns.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 eBlocker Open Source UG (haftungsbeschraenkt)
+ *
+ * Licensed under the EUPL, Version 1.2 or - as soon they will be
+ * approved by the European Commission - subsequent versions of the EUPL
+ * (the "License"); You may not use this work except in compliance with
+ * the License. You may obtain a copy of the License at:
+ *
+ *   https://joinup.ec.europa.eu/page/eupl-text-11-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 package redisdyndns
 
 import (
@@ -14,7 +30,9 @@ import (
 
 const ttl = 60
 
+// Database is a read-only key-value store
 type Database interface {
+	// Query looks up a string value given a query string
 	Query(ctx context.Context, query string) (string, error)
 }
 
@@ -134,6 +152,7 @@ func (rd *RedisDynDns) matches(domain string) bool {
 	return false
 }
 
+// Query implements interface Database
 func (rdb *RedisDatabase) Query(ctx context.Context, query string) (string, error) {
 	return rdb.Client.Get(ctx, query).Result()
 }
